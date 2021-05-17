fa49de54b768cdfddfc129c8e3b7febaec4c060e
tateyama-hiroaki <user@USERnoMacBook-Air.local>
2021-05-17 21:56:18 +0900

    Learned how to use let, subject

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -85,6 +85,134 @@
 #   end
 # end
 
+# RSpec.describe User do
+#   describe '#greet' do
+#     before do
+#       @params = { name: 'たろう' }
+#     end
+#     context '12歳以下の場合' do
+#       before do
+#         @params.merge!(age: 12)
+#       end
+#       it 'ひらがなで答えること' do
+#         user = User.new(@params)
+#         expect(user.greet).to eq 'ぼくはたろうだよ。'
+#       end
+#     end
+#     context '13歳以上の場合' do
+#       before do
+#         @params.merge!(age: 13)
+#       end
+#       it '漢字で答えること' do
+#         user = User.new(@params)
+#         expect(user.greet).to eq '僕はたろうです。'
+#       end
+#     end
+#   end
+# end
+
+# RSpec.describe User do
+#   describe '#greet' do
+#     let(:params) { { name: 'たろう' } }
+#     context '12歳以下の場合' do
+#       before do
+#         params.merge!(age: 12)
+#       end
+#       it 'ひらがなで答えること' do
+#         user = User.new(params)
+#         expect(user.greet).to eq 'ぼくはたろうだよ。'
+#       end
+#     end
+#     context '13歳以上の場合' do
+#       before do
+#         params.merge!(age: 13)
+#       end
+#       it '漢字で答えること' do
+#         user = User.new(params)
+#         expect(user.greet).to eq '僕はたろうです。'
+#       end
+#     end
+#   end
+# end
+
+# RSpec.describe User do
+#   describe '#greet' do
+#     let(:user) { User.new(params) }
+#     let(:params) { { name: 'たろう' } }
+#     context '12歳以下の場合' do
+#       before do
+#         params.merge!(age: 12)
+#       end
+#       it 'ひらがなで答えること' do
+#         expect(user.greet).to eq 'ぼくはたろうだよ。'
+#       end
+#     end
+#     context '13歳以上の場合' do
+#       before do
+#         params.merge!(age: 13)
+#       end
+#       it '漢字で答えること' do
+#         expect(user.greet).to eq '僕はたろうです。'
+#       end
+#     end
+#   end
+# end
+
+# RSpec.describe User do
+#   describe '#greet' do
+#     let(:user) { User.new(params) }
+#     let(:params) { { name: 'たろう', age: age } }
+#     context '12歳以下の場合' do
+#       let(:age) { 12 }
+#       it 'ひらがなで答えること' do
+#         expect(user.greet).to eq 'ぼくはたろうだよ。'
+#       end
+#     end
+#     context '13歳以上の場合' do
+#       let(:age) { 13 }
+#       it '漢字で答えること' do
+#         expect(user.greet).to eq '僕はたろうです。'
+#       end
+#     end
+#   end
+# end
+
+# RSpec.describe User do
+#   describe '#greet' do
+#     let(:user) { User.new(params) }
+#     let(:params) { { name: 'たろう', age: age } }
+#     subject { user.greet }
+#     context '12歳以下の場合' do
+#       let(:age) { 12 }
+#       it 'ひらがなで答えること' do
+#         is_expected.to eq 'ぼくはたろうだよ。'
+#       end
+#     end
+#     context '13歳以上の場合' do
+#       let(:age) { 13 }
+#       it '漢字で答えること' do
+#         is_expected.to eq '僕はたろうです。'
+#       end
+#     end
+#   end
+# end
+
+# RSpec.describe User do
+#   describe '#greet' do
+#     let(:user) { User.new(params) }
+#     let(:params) { { name: 'たろう', age: age } }
+#     subject { user.greet }
+#     context '12歳以下の場合' do
+#       let(:age) { 12 }
+#       it { is_expected.to eq 'ぼくはたろうだよ。' }
+#     end
+#     context '13歳以上の場合' do
+#       let(:age) { 13 }
+#       it { is_expected.to eq '僕はたろうです。' }
+#     end
+#   end
+# end
+
 class User
   def initialize(name:, age:)
     @name = name
@@ -101,26 +229,15 @@ end
 
 RSpec.describe User do
   describe '#greet' do
-    before do
-      @params = { name: 'たろう' }
-    end
+    let(:user) { User.new(name: 'たろう', age: age) }
+    subject { user.greet }
     context '12歳以下の場合' do
-      before do
-        @params.merge!(age: 12)
-      end
-      it 'ひらがなで答えること' do
-        user = User.new(@params)
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
-      end
+      let(:age) { 12 }
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
     context '13歳以上の場合' do
-      before do
-        @params.merge!(age: 13)
-      end
-      it '漢字で答えること' do
-        user = User.new(@params)
-        expect(user.greet).to eq '僕はたろうです。'
-      end
+      let(:age) { 13 }
+      it { is_expected.to eq '僕はたろうです。' }
     end
   end
 end
