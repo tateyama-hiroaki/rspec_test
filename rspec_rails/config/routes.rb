b43d1a7daf47744814c562fe78ebf97f999198fc
tateyama-hiroaki <user@USERnoMacBook-Air.local>
2021-05-28 04:18:35 +0900

    Created rspec_rails

---
+++ rspec_rails/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'users/home'
+  get 'users/help'
+  get 'users/about'
+  get 'users/contact'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
